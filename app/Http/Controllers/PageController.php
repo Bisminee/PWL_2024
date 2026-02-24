07c950507837198e1520b690c57abc3ff41d0d02
Bisminee <fiksilmiah@gmail.com>
2026-02-24 07:17:53 +0700

    controller

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
+
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+
+}
---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function hello(){
+        return 'Hello World';
+    }
+
+}
